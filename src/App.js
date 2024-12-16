f89b11b6dfd907b2b75caf5ab32192256af9cb34
aidarkhusnutdinov <da-scoo@ya.ru>
2024-12-17 01:32:50 +0300

    3 plashki

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
         <div className="overlay">
           <h1>Welcome to Space</h1>
         </div>
+        <div className="overlay2">
+          <h1>Enjoy the stars</h1>
+        </div>
+        <div className="overlay3">
+          <h1>And try not to get too cold...</h1>
+        </div>
       </header>
     </div>
   );
